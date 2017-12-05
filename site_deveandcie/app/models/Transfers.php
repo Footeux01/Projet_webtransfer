2fdc1d8700a0f138be5bac98129cd2facc8cb697
BlackSpirit03 <d.leger@aformac-vichy.fr>
2017-12-05 16:40:50 +0100

    Update Models
    Update Models

---
+++ site_deveandcie/app/models/Transfers.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transfers extends Model
+{
+    protected $table = 'Transfer';
+    public $timestamps = true;
+
+}
