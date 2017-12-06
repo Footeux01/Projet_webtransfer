45d8dc710c139ff8faa37551f3c614c62f3a8d0f
Steve <Lormelet01@hotmail.fr>
2017-12-06 12:28:14 +0100

    x

--- site_deveandcie/resources/views/welcome.blade.php
+++ site_deveandcie/resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>Welcome !</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="{{asset(img/logo.ico')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
 </head>
 <body>
