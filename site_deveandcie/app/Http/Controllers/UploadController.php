e783d6d31f38e0c6bf9616642d08debe11325415
BlackSpirit03 <d.leger@aformac-vichy.fr>
2017-12-07 09:07:22 +0100

    Ajout de commentaire dans le controlleur UpdateController
    Ajout de commentaire dans le controlleur UpdateController

--- site_deveandcie/app/Http/Controllers/UploadController.php
+++ site_deveandcie/app/Http/Controllers/UploadController.php
@@ -12,19 +12,22 @@ class UploadController extends Controller
     //
     public function send(Request $request)
     {	
-        //Incription dans la database
+        //Incription de la demande dans la database
         $transfer = new Transfers($request->except('csrf_token'));
         $transfer->save();
         
     	
-        //Copier le fichier
+        //Enregistrement du fichier sélectionné
         //$path = Storage::disk('upload')->store($request->file_name);
         $path = $request->file_name->store('/', 'upload');
         dd($path);
     	
+        //Envoi du mail avec le lien vers le fichier
+    	
 
-    	return $request->input('file_name') . ' ' . $request->input('exp_mail') . ' ' . $request->input('dest_mail') . ' ' . $request->input('dest_message');
-    	// return view('confirmup');
+        //Test du retour des champs du formulaire (à virer)
+        return $request->input('file_name') . ' ' . $request->input('exp_mail') . ' ' . $request->input('dest_mail') . ' ' . $request->input('dest_message');
+    	   // return view('confirmup');
 
     }
 }
